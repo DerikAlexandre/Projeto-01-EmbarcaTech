14725baa0d114f1ed2ac36b9e4b71f5121dd49af
DerikAlexandre <derik.alexandre123@gmail.com>
2024-12-18 21:35:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
+int main(){
 
+  int opcao;
+
+    printf("=====MENU=====\n");
+      printf("Insira a opcao que deseja:\n");
+        scanf("%d", &opcao);
+    printf("==============\n");
+}
