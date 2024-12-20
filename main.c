982391d894d9dc9c4e6e1f47700c188acc22657b
Júnior Rodrigues <nivaldojunior037@gmail.com>
2024-12-20 19:57:04 -0300

    Enviando o código com implemento da conversão de temperatura

--- main.c
+++ main.c
@@ -6,7 +6,58 @@ void massa(){}
 
 void volume(){}
 
-void temperatura(){}
+void temperatura(float valor){
+
+  int selecao;
+  float result;
+  printf("\n\n===Insira a conversao que deseja realizar:===\n\n");
+  printf("1-Celsius para Fahrenheit\n");
+  printf("2-Celsius para Kelvin\n");
+  printf("3-Fahrenheit para Celsius\n");
+  printf("4-Fahrenheit para Kelvin\n");
+  printf("5-Kelvin para Celsius\n");
+  printf("6-Kelvin para Fahrenheit\n");
+  printf("\n====================\n");
+  scanf("%d", &selecao);
+
+  switch(selecao){
+    case 1: 
+    result = valor*9/5+32;
+    printf("\nValor convertido: %.2fF", result);
+    break;
+
+    case 2: 
+    result = valor+273.15;
+    printf("\nValor convertido: %.2fK", result);
+    break;
+
+    case 3: 
+    result = (valor-32)*5/9;
+    printf("\nValor convertido: %.2fC", result);
+    break;
+
+    case 4: 
+    result = (valor-32)*5/9+273.15;
+    printf("\nValor convertido: %.2fK", result);
+    break;
+
+
+    case 5: 
+    result = valor-273-15;
+    printf("\nValor convertido: %.2fC", result);
+    break;
+
+    case 6: 
+    result = (valor-273.15)*9/5+32;
+    printf("\nValor convertido: %.2fF", result);
+    break;
+
+    default:
+    printf("\nValor invalido!");
+    break;
+  }
+
+}
 
 void tempo(){}
 
